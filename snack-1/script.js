cb0f968097aa179e55ed4fcaf88a1af00320264f
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-15 17:28:49 +0100

    Snack 1 - Mostro a schermo i risultati

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,6 +3,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+const container= document.getElementById("risultato");
+console.log(risultato);
 names.forEach(element => {
-    console.log(element);
+    const risultato = document.createElement('div');
+    risultato.innerHTML = `<P> ${element} </P>`;
+    container.appendChild(risultato);
 });
